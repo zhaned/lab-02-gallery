14e7e445800047aae733ea6f83fba4a1670bd4a3
zhaned <zhanedmond@gmail.com>
2021-02-10 23:03:07 -0800

    refactored code/new dropdown

--- src/App.js
+++ src/App.js
@@ -4,46 +4,66 @@ import React from 'react';
 import Data from './data.js';
 import ImageList from './ImageList.js';
 import Header from './Header.js';
+import Dropdown from './Dropdown.js';
 
 
 export default class App extends React.Component {
   state = {
-    keyword: ''
+    keyword: '',
+    horns: '',
+  }
+
+  handleKeywordChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
+  }
+
+  handleHornsChange = (e) => {
+    this.setState({
+      horns: Number(e.target.value)
+    })
   }
 
   render(){
+    let theKey = this.state.keyword;
+    let theHorn = this.state.horns;
     const filteredImageList = Data.filter((image) =>{
-        if (!this.state.keyword) return true; 
-        if (image.keyword === this.state.keyword) return true;
+        if (!theKey && !theHorn) return true; 
 
+        if(theKey && !theHorn){
+          if (image.keyword === theKey) return true;
+        }
+
+        if(!theKey && theHorn){
+          if(image.horns === theHorn) return true;
+        }
+
+        if(theKey && theHorn){
+          if(image.keyword === theKey && image.horns === theHorn) return true;
+        }
+        
         return false;
       });
+
 
     return(
       <div>
         <Header />
 
-      <form>
         <label>keyword:{' '}</label>
-          <select
-            value={this.state.keyword}
-            onChange={(e) => {
-              this.setState({
-                keyword: e.target.value
-              })
-            }}
-          >
-            <option value="narwhal">narwhal</option>
-            <option value="rhino">rhino</option>
-            <option value="mouflon">mouflon</option>
-            <option value="markhor">markhor</option>
-            <option value="triceratops">triceratops</option>
-            <option value="addax">addax</option>
-            <option value="chameleon">chameleon</option>
-            <option value="lizard">lizard</option>
-            <option value="dragon">dragon</option>
-          </select>
-      </form>
+        <Dropdown 
+        currentValue={this.state.keyword}
+        handleChange={this.handleKeywordChange}
+        options={['narwhal', 'rhino', 'mouflon', 'markhor', 'triceratops', 'addax', 'chameleon', 'lizard', 'dragon']}
+        />
+
+        <label>horns:{' '}</label>
+        <Dropdown
+        currentValue={this.state.horns}
+        handleChange={this.handleHornsChange}
+        options={[1, 2, 3, 100]}
+        />
       <ImageList images={filteredImageList} />
       </div>
     )
